e75098c76d2a0e03dbe983feb07e409bde51ad8c
Joanna Lee <joannalee@google.com>
2020-05-08 14:55:49 +1000

    Call delete API in hover dropdown menu

--- assets/src/edit-story/components/library/panes/media/dropDownMenu.js
+++ assets/src/edit-story/components/library/panes/media/dropDownMenu.js
@@ -19,7 +19,7 @@
  */
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { useCallback, useRef } from 'react';
+import { useCallback, useState, useRef } from 'react';
 
 /**
  * WordPress dependencies
@@ -29,9 +29,10 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
+import { More } from '../../../../components/button';
 import DropDownList from '../../../../components/form/dropDown/list';
 import Popup from '../../../../components/popup';
-import { More } from '../../../../components/button';
+import DeleteDialog from './deleteDialog';
 
 const MoreButton = styled(More)`
   position: absolute;
@@ -59,6 +60,7 @@ function DropDownMenu({ resource, pointerEntered, isMenuOpen, setIsMenuOpen }) {
     { name: __('Delete', 'web-stories'), value: 'delete' },
   ];
 
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
   const moreButtonRef = useRef();
 
   const onClickMoreIcon = useCallback(() => setIsMenuOpen(true), [
@@ -72,7 +74,7 @@ function DropDownMenu({ resource, pointerEntered, isMenuOpen, setIsMenuOpen }) {
         // TODO(#354): Edit Media Metadata via Media Library Hover Menu
         break;
       case 'delete':
-        // TODO(#1319): Media Library - Delete via Dropdown Menu from Hover
+        setShowDeleteDialog(true);
         break;
       default:
         break;
@@ -86,28 +88,38 @@ function DropDownMenu({ resource, pointerEntered, isMenuOpen, setIsMenuOpen }) {
 
   // Keep icon and menu displayed if menu is open (even if user's mouse leaves the area).
   return (
-    !resource.local && // Don't show menu if resource not uploaded to server yet.
-    (pointerEntered || isMenuOpen) && (
-      <>
-        <MoreButton
-          ref={moreButtonRef}
-          width="28"
-          height="28"
-          onClick={onClickMoreIcon}
-          aria-pressed={isMenuOpen}
-          aria-haspopup={true}
-          aria-expanded={isMenuOpen}
-        />
-        <Popup anchor={moreButtonRef} isOpen={isMenuOpen}>
-          <DropDownContainer>
-            <DropDownList
-              handleCurrentValue={handleCurrentValue}
-              options={options}
-              toggleOptions={toggleOptions}
+    !resource.local && ( // Don't show menu if resource not uploaded to server yet.
+      <div>
+        {(pointerEntered || isMenuOpen) && (
+          <>
+            <MoreButton
+              ref={moreButtonRef}
+              width="28"
+              height="28"
+              onClick={onClickMoreIcon}
+              aria-pressed={isMenuOpen}
+              aria-haspopup={true}
+              aria-expanded={isMenuOpen}
             />
-          </DropDownContainer>
-        </Popup>
-      </>
+            <Popup anchor={moreButtonRef} isOpen={isMenuOpen}>
+              <DropDownContainer>
+                <DropDownList
+                  handleCurrentValue={handleCurrentValue}
+                  options={options}
+                  toggleOptions={toggleOptions}
+                />
+              </DropDownContainer>
+            </Popup>
+          </>
+        )}
+        {showDeleteDialog && (
+          <DeleteDialog
+            mediaId={resource.id}
+            showDeleteDialog={showDeleteDialog}
+            setShowDeleteDialog={setShowDeleteDialog}
+          />
+        )}
+      </div>
     )
   );
 }
